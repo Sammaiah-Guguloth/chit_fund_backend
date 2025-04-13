0336dfc75de795c52cf12c711abd5f1e7fa813d0
Sammaiah-Guguloth <gugulothsammaiah5@gmail.com>
2025-04-13 21:11:07 +0530

    changed port and instansited server in index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const connectToDb = require("./config/connectToDb");
 const adminRoutes = require("./routes/admin.routes");
 const userRoutes = require("./routes/user.routes");
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
@@ -20,6 +20,10 @@ app.use("/api/v1/user", userRoutes);
 app.use(express.urlencoded({ extended: true }));
 
 connectToDb();
+
+app.listen(port, () => {
+  console.log("app is listening on port : ", port);
+});
 
 app.get("/", (req, res) => {
   res.send("<center> <h1> Welcome to Chit Fund  </h1> </center>");
